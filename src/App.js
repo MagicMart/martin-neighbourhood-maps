d248d143ef23c69f954d27d471861808abb5bb63
Martin Tudor <martcan27@gmail.com>
2018-07-13 21:22:21 +0100

    Add component SearchResults

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMaps from './components/GoogleMaps.js'
 import SearchBox from './components/Sidebar'
+import SearchResults from './components/SearchResults'
 import './App.css';
 
 class App extends Component {
@@ -11,17 +12,19 @@ class App extends Component {
 
   filterPlaces = (val) => {
     this.setState({query: val})
+  
   }
 
   render() {
     return (
       <div className='container'>
-         <div className='search-box'>
+         <div className='sidebar'>
         <h1>Potteries Museums</h1>
         <SearchBox 
         query={this.state.query}
         filter={this.filterPlaces}
         />
+        <SearchResults id='search-results'/>
         </div>
         <GoogleMaps id='map'/>
       </div>
