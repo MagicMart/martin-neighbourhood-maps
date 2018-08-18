02b8586ca6011b47df1e8947614ccc85b8478495
Martin Tudor <martcan27@gmail.com>
2018-08-19 00:25:07 +0100

    Make hamburger icon an svg

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import hamburger from "../hamburger.svg";
 
 function Hamburger(props) {
+  const style = {
+    maxHeight: "48px",
+    padding: "5px"
+    // margin: "6px 0"
+  };
+
   return (
     <div id="hamburger-bar">
       <div className="bap" onClick={props.hamburgerClick} role="navigation">
+        {/* <div className="hamburger" />
         <div className="hamburger" />
-        <div className="hamburger" />
-        <div className="hamburger" />
+        <div className="hamburger" /> */}
+        <img src={hamburger} style={style} />
       </div>
     </div>
   );
